c09f3ee5debb418a58da7bb21776e55c4ff4d504
tariqul1989 <tariqul@yahoo.com>
2016-12-28 18:58:46 +0600

    exam pro

--- js/app.js
+++ js/app.js
@@ -11,11 +11,38 @@ $(document).ready(function(){
 
 //for image gallery
 
-$(".nxt").click(function(){
-
-	$(".img2").css("border","2px solid red");
-});
-
+//for image gallery start
+$(function () {
+    $("#project-des p:eq(0)").nextAll().hide();
+    $(".small-images img").click(function (e) {
+        var $this = $(this),
+            index = $this.index();
+        $(".small-images img").removeClass('selected');
+        $this.addClass('selected');
+        $("#project-des p").eq(index).show().siblings().hide();
+    });
+    $('#next').click(function(){ 
+        var $selected = $('.selected');
+        var index = $selected.next('img').index();
+        if(index == -1){
+            index = 0;
+        }
+        var $curr = $(".small-images img").eq(index);
+        $(".small-images img").removeClass('selected');
+        $curr.addClass('selected');
+        $("#project-des p").eq(index).show().siblings().hide();
+    });
+    
+    $('#prev').click(function(){ 
+        var $selected = $('.selected');
+        var index = $selected.prev('img').index();
+        var $curr = $(".small-images img").eq(index);
+        $(".small-images img").removeClass('selected');
+        $curr.addClass('selected');
+        $("#project-des p").eq(index).show().siblings().hide();
+    });
+}); 
+//for image gallery End
 
    
 }); 
